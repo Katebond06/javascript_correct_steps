d5393c0a0d1eaae22ac1b7448502f8a56b62111f
kate <bkateryna27@icloud.com>
2020-04-18 19:09:28 -0400

    methods

--- js.js
+++ js.js
@@ -10,3 +10,12 @@ console.log(isAGirl);
 
 var age;
 console.log(age);
+
+// will give you alert; another alert will give you an answer to the meth brob you've created
+// alert("Hello there!");
+// alert(198 * 345);
+
+// will ask the user a question where he needs to answer something
+prompt("what is your name?");
+
+var userName = prompt("what is your name?");
