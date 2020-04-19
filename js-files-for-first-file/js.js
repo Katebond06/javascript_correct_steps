78c54518560c1c2c36937f99fa7ffbb881e7814a
kate <bkateryna27@icloud.com>
2020-04-19 19:27:33 -0400

    operators

---
+++ js-files-for-first-file/js.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+
+// var yourName = "Kate";
+// var yourAge = 26;
+// var isAGirl = true;
+
+// yourName = "MissKitKa";
+// console.log(yourName);
+// console.log(isAGirl);
+
+// var age;
+// console.log(age);
+
+// will give you alert; another alert will give you an answer to the meth brob you've created
+// alert("Hello there!");
+// alert(198 * 345);
+
+// will ask the user a question where he needs to answer something
+// prompt("what is your name?");
+
+// var userName = prompt("what is your name?");
+// var userName = prompt("What is your name");
+// alert("Nice to meet you, " + userName);
+
+var userName = prompt("What is your name?");
+var userLastName = prompt("What is your last mame?");
+var userAge = prompt("How old are you?");
+
+var fullName = userName + " " + userLastName;
+
+// console.log("Your full name is " + userName + " " + userLastName);
+console.log("Your full name is " + fullName);
+
+console.log("You are " + userAge + " years old");
---
+++ js-files-for-first-file/secjs.js
@@ -0,0 +1,6 @@
+console.log("Hey!");
+
+var userAge = prompt("How old are you?");
+var daysOnPlanet = userAge * 365;
+
+alert("It is roughly " + daysOnPlanet);
