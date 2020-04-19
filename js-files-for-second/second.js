7b4cc46f502e5c0d03260418da2e17005dc3846f
kate <bkateryna27@icloud.com>
2020-04-19 19:49:51 -0400

    conditions

--- js-files-for-second/second.js
+++ js-files-for-second/second.js
@@ -1,1 +1,15 @@
 console.log("Hi!");
+
+var x = 10;
+var y = "a";
+
+y === "b" || x >= 10;
+
+// console.log(y === "b" || x >= 10);
+
+var x = 3;
+var y = 8;
+
+!(x == "3" || x === y) && !(y != 8 && x <= y);
+
+// console.log(!(x == "3" || x === y) && !(y != 8 && x <= y));
