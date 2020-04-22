3b9e792fed47ecf5392fd7406b2aa2f1b3e7ce66
kate <bkateryna27@icloud.com>
2020-04-21 20:23:23 -0400

    start loops

---
+++ Loops/loops.js
@@ -0,0 +1,8 @@
+console.log("Hello!");
+
+var count = 1;
+//  print number from 1 - 5
+while (count < 6) {
+  console.log("count is: " + count);
+  count++;
+}
--- js-conditions/conditions.js
+++ js-conditions/conditions.js
@@ -1,6 +1,6 @@
 console.log("Hello");
 
-var age = Number(prompt("How old are you?"));
+// var age = Number(prompt("How old are you?"));
 
 // if (age < 18) {
 //   alert("You cannot come in!");
@@ -10,15 +10,51 @@ var age = Number(prompt("How old are you?"));
 //   alert("U can come in and U can drink! Yey!");
 // }
 
-if (age < 0) {
-  alert("ERROR! You are not even born yet!");
+// if (age < 0) {
+//   alert("ERROR! You are not even born yet!");
+// }
+// if (age === 21) {
+//   alert("Happy 21st Birthday!");
+// }
+// if (age % 2 !== 0) {
+//   alert("Your age is odd");
+// }
+// if (age % Math.sqrt(age) === 0) {
+//   alert("Your age is a perfect square!");
+// }
+
+// Guessing Game
+
+// creat a secret number;
+// ask user a secret number
+// check guess
+
+var secretNumber = 4;
+
+// sample
+// var guess = Number(prompt("Guess a number"));
+
+// other way
+
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+
+// check if guess is right
+
+if (guess === secretNumber) {
+  alert("You guessed it right!");
 }
-if (age === 21) {
-  alert("Happy 21st Birthday!");
+// otherwise
+// else {
+//   alert("Wrong!");
+// }
+
+// check if guess is higher >
+else if (guess > secretNumber) {
+  alert("Too high. Guess again!");
 }
-if (age % 2 !== 0) {
-  alert("Your age is odd");
-}
-if (age % Math.sqrt(age) === 0) {
-  alert("Your age is a perfect square!");
+
+// check if guess is too lowe
+else {
+  alert("Too low. Guess again!");
 }
